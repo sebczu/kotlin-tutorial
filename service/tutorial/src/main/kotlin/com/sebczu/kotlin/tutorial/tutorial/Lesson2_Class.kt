ecfafcc345b37d1beaaf379ea11559145274de66
dev.sebastian.czubala <dev.sebastian.czubala@gmail.com>
2022-03-06 17:48:04 +0100

    kotlin tutorial

---
+++ service/tutorial/src/main/kotlin/com/sebczu/kotlin/tutorial/tutorial/Lesson2_Class.kt
@@ -0,0 +1,24 @@
+package com.sebczu.kotlin.tutorial.tutorial
+
+fun main(args: Array<String>) {
+    var person = Person("name", "email")
+
+    println("name ${person.name} email ${person.email}")
+    person.name = "new"
+    println("name ${person.name} email ${person.email}")
+//    not allowed, val - const
+//    obj.email = "new"
+
+    var person2 = Person2("name")
+    println("name ${person2.name}")
+
+}
+
+class Person(var name: String, val email: String)
+
+class Person2(val name: String) {
+
+    constructor(name: String, email: String) : this(name) {
+        println("second constructor")
+    }
+}
