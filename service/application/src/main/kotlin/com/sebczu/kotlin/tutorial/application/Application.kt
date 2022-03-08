286c5ab6aaf4c79ee67dc3f6ebdeed45a9de3505
dev.sebastian.czubala <dev.sebastian.czubala@gmail.com>
2022-03-08 21:41:30 +0100

    kotlin tutorial

---
+++ service/application/src/main/kotlin/com/sebczu/kotlin/tutorial/application/Application.kt
@@ -0,0 +1,11 @@
+package com.sebczu.kotlin.tutorial.application
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
---
+++ service/application/src/main/kotlin/com/sebczu/kotlin/tutorial/application/test/TestController.kt
@@ -0,0 +1,16 @@
+package com.sebczu.kotlin.tutorial.application.test
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/test")
+class TestController {
+
+    @GetMapping
+    fun test(): String {
+        return "test";
+    }
+
+}
